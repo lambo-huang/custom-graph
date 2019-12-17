39ca27c6ef9997da5596ad369d446a06a7ad7f18
huanglianbiao-dev <1139992048@qq.com>
2019-12-17 16:33:20 +0800

    自定义节点 + 自动布局关系树图实现

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h3>JsPlumb + D3js实现自定义节点，自动树状布局</h3>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  height: 100%;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
